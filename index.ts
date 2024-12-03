9f5528a51749d63bc5e1bfd5ff5de6576f6d3e14
rei666 <h1.tnguyen088@gmail.com>
2024-12-03 20:16:24 +0700

    Project init

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
+
+app.get("/tours", (req: Request, res: Response) => {
+    res.render("client/pages/tours/index");
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
