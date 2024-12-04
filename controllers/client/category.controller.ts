72bd2f186b004c934ed54a899fe1be5680416514
rei666 <h1.tnguyen088@gmail.com>
2024-12-04 21:33:55 +0700

    fix bug

--- controllers/client/category.controller.ts
+++ controllers/client/category.controller.ts
@@ -1,18 +1,18 @@
-import {Response, Request} from "express";
+import { Request, Response } from "express";
 import Category from "../../models/category.model";
 
-export const index = async (res: Response, req: Request) => {
-    //* SELECT * FROM categories WHERE deleted = false AND status = "active";
+export const index = async (req: Request, res: Response) => {
+    //* SELECT * FROM category WHERE deleted = flase AND status = "active";
     const categories = await Category.findAll({
         where: {
-            deleted: false,
-            status: "active"
+          deleted: false,
+          status: "active"
         },
         raw: true
     });
 
     res.render("client/pages/categories/index", {
-        pageTitle: "Tour Categories",
+        pageTitle: "All category",
         categories: categories
     });
 }
